56e595e27ccb16134695aef26f176a527013d553
zaruyashar <203256006+zaruyashar@users.noreply.github.com>
2026-08-08 14:24:46 +0300

    Project 10 - Text to Speech with Speech.Synthesis added

---
+++ NetCoreAI.Project10_TextToSpeech/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Speech.Synthesis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+
+        speechSynthesizer.Volume = 100;
+        speechSynthesizer.Rate = 1; // The speed of speech.
+        speechSynthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
+
+        Console.WriteLine("Enter the text you want to convert to speech: ");
+        string userInput = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(userInput))
+        {
+            speechSynthesizer.Speak(userInput);
+        }
+    }
+}
