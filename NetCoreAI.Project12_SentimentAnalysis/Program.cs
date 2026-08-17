5c123112bde67f2107b8e55d3ac8489c750f24ca
zaruyashar <203256006+zaruyashar@users.noreply.github.com>
2026-08-17 20:32:19 +0300

    Sub-project naming convention revised

---
+++ NetCoreAI.Project12_SentimentAnalysis/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    private static string apiKey;
+
+    static async Task Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+
+        apiKey = config["OpenAI:ApiKey"];
+
+        Console.WriteLine("Enter the text you want to convert to speech: ");
+        string userInput = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(userInput))
+        {
+
+        }
+    }
+}
